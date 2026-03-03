273f0d689e5cf695e9e13297749da61bcb3bd810
Alejandro Ibarra <alejandro.ibarra@cakedc.com>
2026-03-03 15:07:40 +0100

    Update spa

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,6 +18,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    allowedHosts: [
+      'triggertime.ddev.site'
+    ],
     hmr: {
       host: 'triggertime.ddev.site',
       protocol: 'wss',
