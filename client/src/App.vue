8d55cbc20ab31a377c74ecb287a4f276e738fe16
Alejandro Ibarra <alejandro.ibarra@cakedc.com>
2026-03-03 09:36:18 +0100

    fix: remove unused ref import from App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useAuthStore } from '@/stores/auth'
